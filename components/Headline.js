b1352e7f30df5a73a8e38aea64251d7bff1e159a
kyun <fm13071303@rx.tnc.ne.jp>
2023-10-12 13:36:12 +0900

    💄 スタイルファイルの分割

--- components/Headline.js
+++ components/Headline.js
@@ -1,44 +1,42 @@
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
-
+import classes from './Headline.module.css';
 
 export function Headline(props) {
   return (
     <>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/{props.page}.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <div className={classes.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={classes.code}>pages/{props.page}.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={classes.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
+      </div>
+      <div className={classes.center}>
+        <Image
+          className={classes.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
     </>
   );
 }
--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,13 @@
 import { Headline } from '../components/Headline';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import classes from './Main.module.css';
 import { Links } from '../components/Links';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export function Main(props) {
   return (
-    <main className={`${styles.main} ${inter.className}`}>
+    <main className={`${classes.main} ${inter.className}`}>
       <Headline page={props.page} />
 
       <Links />
