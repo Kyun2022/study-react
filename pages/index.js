6dd86305ee11444fedf40ef90972d0a5a3a65569
kyun <fm13071303@rx.tnc.ne.jp>
2023-10-12 10:53:02 +0900

    ♻️  ヘッドライン部分をコンポーネント化

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import { Headline } from '../components/Headline';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import { Links } from '../components/Links';
@@ -17,39 +17,7 @@ export default function Index() {
       </Head>
 
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        <Headline page="index" />
 
         <Links />
       </main>
